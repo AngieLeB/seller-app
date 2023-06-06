2d6d01378660958831bc7015de6862f67c47861a
Angélique Le bail <angelique.lebail@cdbdx.biz>
2023-06-06 15:58:44 +0200

    feat: 🎸 add header and ThemeProvider

--- src/App/index.js
+++ src/App/index.js
@@ -1,7 +1,13 @@
-import Counter from "../Components/Counter";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import theme from "./theme";
+import Header from "../Components/Header";
 
-function App() {
-  return <Counter startValue={1} step={2} />;
+export default function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Header />
+      MAIN
+    </ThemeProvider>
+  );
 }
-
-export default App;
---
+++ src/App/theme.js
@@ -0,0 +1,9 @@
+import { createTheme } from "@mui/material";
+import { blue, green } from "@mui/material/colors";
+
+export default createTheme({
+  palette: {
+    primary: blue,
+    secondary: green,
+  },
+});
