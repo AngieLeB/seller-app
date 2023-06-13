3a7453fb4bd110c66cb542476f1710becf392872
Angélique Le bail <angelique.lebail@cdbdx.biz>
2023-06-13 14:56:46 +0200

    test: 💍 add counter unit tests

--- src/Components/Counter/index.test.js
+++ src/Components/Counter/index.test.js
@@ -3,15 +3,17 @@ import userEvent from "@testing-library/user-event";
 import Counter from ".";
 
 const counterValue = () => screen.getByLabelText("counterValue");
-const incButton = () => screen.getByRole("button", { name: "+1" });
-const decButton = () => screen.getByRole("button", { name: "-1" });
+const incButton = ({ step = 1 } = {}) =>
+  screen.getByRole("button", { name: `+${step}` });
+const decButton = ({ step = 1 } = {}) =>
+  screen.getByRole("button", { name: `-${step}` });
 
 //test suite
 describe("Counter", () => {
   it("renders counter value, + and - buttons", () => {
     render(<Counter />);
     //assertion
-    expect(counterValue()).toHaveTextContent("0");
+    expect(counterValue()).toHaveTextContent(/^0$/);
     expect(incButton()).toBeInTheDocument();
     expect(decButton()).toBeInTheDocument();
   });
@@ -19,8 +21,39 @@ describe("Counter", () => {
   it("increments and decrements while clicking on +/- buttons", () => {
     render(<Counter />);
     userEvent.click(incButton());
-    expect(counterValue()).toHaveTextContent("1");
+    expect(counterValue()).toHaveTextContent(/^1$/);
     userEvent.click(decButton());
-    expect(counterValue()).toHaveTextContent("0");
+    expect(counterValue()).toHaveTextContent(/^0$/);
+  });
+});
+
+describe("while startValue === 10", () => {
+  it("renders counter value", () => {
+    render(<Counter startValue={10} />);
+    expect(counterValue()).toHaveTextContent(/^10$/);
+  });
+
+  it("increments and decrements while clicking on +/- buttons", () => {
+    render(<Counter startValue={10} />);
+    userEvent.click(incButton());
+    expect(counterValue()).toHaveTextContent(/^11$/);
+    userEvent.click(decButton());
+    expect(counterValue()).toHaveTextContent(/^10$/);
+  });
+});
+
+describe("while step == 4", () => {
+  const step = 4;
+  it("renders counter value", () => {
+    render(<Counter step={step} />);
+    expect(counterValue()).toHaveTextContent(/^0$/);
+  });
+
+  it("increments and decrements while clicking on +/- buttons", () => {
+    render(<Counter step={step} />);
+    userEvent.click(incButton({ step }));
+    expect(counterValue()).toHaveTextContent(/^4$/);
+    userEvent.click(decButton({ step }));
+    expect(counterValue()).toHaveTextContent(/^0$/);
   });
 });
